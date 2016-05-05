15dd7988317e7d64be6d122e4e56cd4f5651fc78
rdelvallej32 <rdelvallej@gmail.com>
2016-05-05 11:48:05 -0400

    add event handlers to index file when DOM is ready

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 require('./example');
+const eventHandlers = require('./events/handlers');
 
 // on document ready:
 
 $(() => {
   console.log('Page loaded!');
+  eventHandlers.addHandlers();
 });
