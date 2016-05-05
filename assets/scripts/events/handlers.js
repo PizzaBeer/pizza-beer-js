8f5e0c2426db8fb7ae3111057667cb2c03a6385a
rdelvallej32 <rdelvallej@gmail.com>
2016-05-05 11:47:31 -0400

    add event handler function to invoke get ajax call

--- assets/scripts/events/handlers.js
+++ assets/scripts/events/handlers.js
@@ -1,6 +1,16 @@
 'user strict';
 
+const app = require('../app-data.js');
+const authApi = require('../auth/user-api.js');
+
 //runs ajax call to get users
+const addHandlers = function() {
     $('#get-users').on('click', function(event){
       event.preventDefault();
+      authApi.users();
   });
+};
+
+module.exports= {
+    addHandlers
+  };
